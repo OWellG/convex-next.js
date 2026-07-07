b00b1112ad27bcad20ea43da442c676fa200b9c1
OWellG <owellg126@gmail.com>
2026-07-07 10:47:35 +0200

    linter fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from 'typescript-eslint';
 import js from '@eslint/js';
 export default defineConfig([
-  globalIgnores(["convex/_generated"]),
+  globalIgnores([
+    ".next",
+    "node_modules",
+    "convex/_generated"]),
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
     extends: [js.configs.recommended, tseslint.configs.recommended],
